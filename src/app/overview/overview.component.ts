7d4d4ca44bdf7282b25b3687205d26383dd6c840
Marcus N Ahlin <marcusnahlin@gmail.com>
2025-09-15 14:25:34 +0200

    feat: added basic overview page

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+interface Document {
+  _id: string;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  private api = inject(ApiService);
+  documents: Document[] = [];
+
+  ngOnInit(): void {
+    this.getDocuments();
+  }
+
+  getDocuments(): void {
+    this.api.getDocuments().subscribe((data: Document[]) => {
+      this.documents = data;
+      console.log('Data:', data);
+    });
+  }
 }
