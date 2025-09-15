fa5f7c18951d100a106cb2c7c2d0d0dbdb536048
Marcus N Ahlin <marcusnahlin@gmail.com>
2025-09-15 13:54:43 +0200

    fix: lint fixes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,17 +1,23 @@
 import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
+
+interface document {
+  _id: string,
+  title: string,
+  content: string,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   private apiURL = environment.API_URL;
+  private http = inject(HttpClient);
 
-  constructor(private http: HttpClient) { }
-
-  getDocuments(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiURL + '/docs')
+  getDocuments(): Observable<document[]> {
+    return this.http.get<document[]>(this.apiURL + '/docs');
   }
 }
