4ea640d84f90b48b2ef1c079498348aa2eda12a2
albinr <albin@rybergs.net>
2025-09-16 13:21:53 +0200

    chore: generated doc-edit component

---
+++ src/app/doc-edit/doc-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-edit',
+  imports: [],
+  templateUrl: './doc-edit.component.html',
+  styleUrl: './doc-edit.component.scss'
+})
+export class DocEditComponent {
+
+}
