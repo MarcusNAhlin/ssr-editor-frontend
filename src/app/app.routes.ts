5ba0501cc825dd54b532f3c72963512e7d13d526
albinr <albin@rybergs.net>
2025-09-16 14:03:23 +0200

    feat: added doc edit route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
+import { DocEditComponent } from './doc-edit/doc-edit.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/overview', pathMatch: 'full' },
   { path: 'overview', component: OverviewComponent },
+  { path: 'doc/:id', component: DocEditComponent }
 ];
