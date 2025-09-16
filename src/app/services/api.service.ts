ae6d699425dd9a64efd07f3bef4d6f7a398ea8c7
Marcus N Ahlin <marcusnahlin@gmail.com>
2025-09-16 14:06:48 +0200

    feat: added ability to add a new document

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -23,6 +23,12 @@ export class ApiService {
     );;
   }
 
+  addDocument(documentData: document): Observable<document> {
+    return this.http.post<document>(this.apiURL + '/docs/add', documentData).pipe(
+      catchError(this.handleError)
+    );;
+  }
+
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
