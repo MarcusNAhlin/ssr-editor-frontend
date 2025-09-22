848d8ebebe15c8b39b76d697645e514c5775e473
Marcus N Ahlin <marcusnahlin@gmail.com>
2025-09-22 16:41:02 +0200

    test: added unit tests for document card

--- src/app/components/document-card/document-card.component.spec.ts
+++ src/app/components/document-card/document-card.component.spec.ts
@@ -1,23 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { DocumentCardComponent } from './document-card.component';
 
-describe('DocumentCardComponent', () => {
+const mockDocument = {
+  _id: '123',
+  title: 'Test Document Title',
+  content: 'Test document content.'
+};
+
+fdescribe('DocumentCardComponent', () => {
   let component: DocumentCardComponent;
   let fixture: ComponentFixture<DocumentCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DocumentCardComponent]
-    })
-      .compileComponents();
+      imports: [DocumentCardComponent],
+      providers: [provideRouter([])]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DocumentCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('TC01 - should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('TC02 - should display document title if provided', () => {
+    component.document = mockDocument;
+    fixture.detectChanges();
+
+    const cardElement = fixture.nativeElement as HTMLElement;
+
+    expect(cardElement.querySelector('h3')?.textContent).toContain('Test Document');
+  });
+
+  it('TC03 - should display doc ID if title is not provided', () => {
+    const docWithNoTitle = { ...mockDocument, title: '' };
+
+    component.document = docWithNoTitle;
+    fixture.detectChanges();
+
+    const cardElement = fixture.nativeElement as HTMLElement;
+
+    expect(cardElement.querySelector('h3')?.textContent).toContain('ID: 123');
+  });
+
+  it('TC04 - should not render if no document', () => {
+    fixture.detectChanges();
+    const cardElement = fixture.nativeElement as HTMLElement;
+
+    expect(cardElement.querySelector('.document')).toBeNull();
+  });
+
+  it('TC05 - should have a link to the document edit page', () => {
+    component.document = mockDocument;
+    fixture.detectChanges();
+
+    const cardElement = fixture.nativeElement as HTMLElement;
+    const link = cardElement.querySelector('a');
+
+    expect(link?.getAttribute('href')).toBe('/doc/123');
+    expect(link?.hasAttribute('disabled')).toBe(false);
   });
 });
