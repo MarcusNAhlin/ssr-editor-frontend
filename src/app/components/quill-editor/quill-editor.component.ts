0fce9132688acbd36c0ffec9074cdc59c4c2750d
albinr <albin@rybergs.net>
2025-10-06 15:06:06 +0200

    feat: created quill editor component

---
+++ src/app/components/quill-editor/quill-editor.component.ts
@@ -0,0 +1,63 @@
+import { Component, AfterViewInit, ElementRef, OnDestroy, ViewChild, Input } from '@angular/core';
+import Quill from 'quill';
+import QuillCursors from 'quill-cursors';
+import * as Y from 'yjs';
+import { SocketIOProvider } from 'y-socket.io';
+import { QuillBinding } from 'y-quill';
+
+Quill.register('modules/cursors', QuillCursors);
+
+@Component({
+  selector: 'app-quill-editor',
+  standalone: true,
+  templateUrl: './quill-editor.component.html',
+  styleUrl: './quill-editor.component.scss'
+})
+export class QuillEditorComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('host', { static: true }) host!: ElementRef<HTMLDivElement>;
+  @Input ({ required: true }) docId!: string;
+
+  private quill!: Quill;
+  private ydoc!: Y.Doc;
+  private provider!: SocketIOProvider;
+  private yText!: Y.Text;
+  private binding!: QuillBinding;
+  readonly userEmail = 'albin@rybergs.net';
+
+  ngAfterViewInit() {
+    this.quill = new Quill(this.host.nativeElement, {
+      theme: 'snow',
+      modules: {
+        toolbar: [['bold', 'italic', 'underline'],
+          [{ header: [1,2,false] }], ['link', 'code-block']],
+        cursors: true,
+        history: { userOnly: true }
+      },
+    });
+
+    this.ydoc = new Y.Doc();
+    this.provider = new SocketIOProvider(
+      'ws://localhost:3000',
+      `${this.docId}`,
+      this.ydoc,
+      {
+
+      }
+    );
+
+    this.provider.awareness.setLocalStateField('user', {
+      name: `${this.userEmail}`, color: '#4A6A8A'
+    });
+
+    this.yText = this.ydoc.getText('rich');
+    this.binding = new QuillBinding(this.yText, this.quill, this.provider.awareness);
+  
+  }
+
+  getHtml(): string { return this.quill.root.innerHTML; }
+  getPlain(): string { return this.yText.toString(); }
+
+  ngOnDestroy() {
+    this.host.nativeElement.innerHTML = '';
+  }
+}
