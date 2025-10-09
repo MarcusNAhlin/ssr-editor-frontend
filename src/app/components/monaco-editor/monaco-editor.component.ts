d84bf2c46be487fb6db97c2debdbe27f04f5b69f
Marcus N Ahlin <marcusnahlin@gmail.com>
2025-10-09 17:36:26 +0200

    feat: added Y.js connection to new  monaco editor

--- src/app/components/monaco-editor/monaco-editor.component.ts
+++ src/app/components/monaco-editor/monaco-editor.component.ts
@@ -1,7 +1,12 @@
+import * as Y from 'yjs';
+import * as monaco from 'monaco-editor';
+import { NuMonacoEditorComponent, NuMonacoEditorEvent } from '@ng-util/monaco-editor';
+import { Component, computed, inject, Input, OnDestroy } from '@angular/core';
+import { WebsocketProvider } from 'y-websocket';
+import { MonacoBinding } from 'y-monaco';
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NuMonacoEditorComponent } from '@ng-util/monaco-editor';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-monaco-editor',
@@ -10,9 +15,70 @@ import { NuMonacoEditorComponent } from '@ng-util/monaco-editor';
   styleUrl: './monaco-editor.component.scss',
   standalone: true
 })
-export class MonacoEditorComponent {
+export class MonacoEditorComponent implements OnDestroy {
   @Input({ required: true }) docId!: string;
   @Input() value = '';
 
-  editorOptions = { theme: 'vs-dark', language: 'typescript' };
+  public editorOptions = {
+    theme: 'vs-dark',
+    language: 'javascript',
+    automaticLayout: true,
+    value: '',
+    readonly: true,
+  };
+
+  private auth = inject(AuthService);
+
+  private editor?: monaco.editor.IStandaloneCodeEditor | monaco.editor.IStandaloneDiffEditor;
+  private yDoc = new Y.Doc();
+  private provider?: WebsocketProvider;
+  private binding?: MonacoBinding;
+  readonly userEmail = computed(() => this.auth.user()?.email ?? 'Anon');
+
+  onEditorEvent(event: NuMonacoEditorEvent): void {
+    if (event.type === 'init' && event.editor) {
+      this.editor = event.editor as unknown as typeof this.editor;
+
+      try {
+        // test if this.editor is of type IStandaloneCodeEditor
+        // y-monaco doesn't support IStandaloneDiffEditor
+        (this.editor as monaco.editor.IStandaloneCodeEditor).getModel();
+      } catch {
+        console.warn('IStandaloneDiffEditor is not supported by y-monaco');
+        return;
+      }
+
+      this.editor = this.editor as monaco.editor.IStandaloneCodeEditor;
+      // disable input when data not loaded yet
+      this.editor.updateOptions({ readOnly: true });
+
+      this.provider = new WebsocketProvider(
+        'ws://localhost:3000', // TODO: should be taken from env
+        this.docId,
+        this.yDoc
+      );
+
+      const yText = this.yDoc.getText('monaco-content');
+
+      // this.provider.awareness.setLocalStateField('user', {
+      //   name: this.userEmail(),
+      //   color: '#d9ff00ff'
+      // });
+
+      this.binding = new MonacoBinding(
+        yText,
+        this.editor.getModel()!,
+        new Set([this.editor]),
+        this.provider.awareness,
+      );
+
+      this.editor.updateOptions({ readOnly: false });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.provider?.destroy();
+    this.binding?.destroy();
+    this.yDoc.destroy();
+  }
 }
