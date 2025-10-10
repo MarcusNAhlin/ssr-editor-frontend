704cc4f6cdc6548ac4dd3c16ee6279f12ac41a0a
Marcus N Ahlin <marcusnahlin@gmail.com>
2025-10-10 12:05:55 +0200

    fix: update WebSocket URL to use secure connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   API_URL: 'https://angular-avengers-backend-eydthshkg0f6gkem.northeurope-01.azurewebsites.net',
-  API_WS_URL: 'ws://angular-avengers-backend-eydthshkg0f6gkem.northeurope-01.azurewebsites.net',
+  API_WS_URL: 'wss://angular-avengers-backend-eydthshkg0f6gkem.northeurope-01.azurewebsites.net',
 };
