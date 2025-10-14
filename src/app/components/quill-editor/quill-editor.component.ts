de4a36fc447a624377745e1bbadad867003f41df
Marcus N Ahlin <marcusnahlin@gmail.com>
2025-10-14 11:20:08 +0200

    refactor: improved modularity

--- src/app/components/quill-editor/quill-editor.component.ts
+++ src/app/components/quill-editor/quill-editor.component.ts
@@ -99,59 +99,13 @@ export class QuillEditorComponent implements AfterViewInit, OnDestroy {
   }
 
   selectTextInComment(commentId: string) {
-    // If already active, unselect it
-    // TODO: Make code more modular and to DRY
     if (this.activeCommentId === commentId) {
-      this.activeCommentId = null;
-      const comment = this.comments.get(commentId);
-      if (!comment) {
-        console.warn('Comment not found:', commentId);
-        return;
-      }
-      const encodedRelPos = comment.get('relativePos');
-      if (!encodedRelPos) {
-        console.warn('No relativePos found for comment:', commentId);
-        return;
-      }
-      const relativePos = Y.decodeRelativePosition(encodedRelPos);
-      const absolutePos = Y.createAbsolutePositionFromRelativePosition(relativePos, this.ydoc);
-
-      // Check if same instance of Y.Text
-      if (absolutePos && absolutePos.type === this.yText) {
-        const index = absolutePos.index;
-        const length = comment.get('selectionLength');
-
-        // Remove formatting
-        this.quill.formatText(index, length, { background: false }, 'user');
-        this.quill.setSelection(index + length);
-      }
-      return;
-    }
-    this.activeCommentId = commentId;
-    const comment = this.comments.get(commentId);
-
-    if (!comment) {
-      console.warn('Comment not found:', commentId);
+      // Deselect if already active
+      this._removeCommentFormatting(commentId);
       return;
     }
 
-    const encodedRelativePos = comment.get('relativePos');
-    if (!encodedRelativePos) {
-      console.warn('No relativePos found for comment:', commentId);
-      return;
-    }
-    // Decode the relative position
-    const relativePos = Y.decodeRelativePosition(encodedRelativePos);
-    const absolutePos = Y.createAbsolutePositionFromRelativePosition(relativePos, this.ydoc);
-    // Check if same instance of Y.Text
-    if (absolutePos && absolutePos.type === this.yText) {
-      const index = absolutePos.index;
-      const length = comment.get('selectionLength');
-
-      // Add background and set selection to END of commented text
-      this.quill.formatText(index, length, { background: '#d9ff0048' }, 'user');
-      this.quill.setSelection(index + length + 1);
-    }
+    this.updateTextFormatting(commentId);
   }
 
   removeComment(commentId: string) {
@@ -168,29 +122,7 @@ export class QuillEditorComponent implements AfterViewInit, OnDestroy {
       return;
     }
 
-    if (this.activeCommentId === commentId) {
-      this.activeCommentId = null;
-    }
-
-    try {
-      // Remove text formatting
-      const encodedRelativePos = comment.get('relativePos');
-      if (!encodedRelativePos) {
-        console.warn('No relativePos found for comment:', commentId);
-      }
-      // Decode relative position
-      const relativePos = Y.decodeRelativePosition(encodedRelativePos);
-      const absolutePos = Y.createAbsolutePositionFromRelativePosition(relativePos, this.ydoc);
-      if (absolutePos && absolutePos.type === this.yText) {
-        const index = absolutePos.index;
-        const length = comment.get('selectionLength');
-        this.quill.formatText(index, length, { background: false }, 'user');
-        this.quill.setSelection(index + length);
-      }
-    } catch (err) {
-      console.error('Failed to remove comment formatting:', err);
-    }
-
+    this.updateTextFormatting(commentId);
     this.comments.delete(commentId);
   }
 
@@ -198,5 +130,96 @@ export class QuillEditorComponent implements AfterViewInit, OnDestroy {
     this.provider?.destroy();
     this.ydoc?.destroy();
     this.host.nativeElement.innerHTML = '';
+  }
+
+  private updateTextFormatting(commentId: string) {
+    const comment = this.comments.get(commentId);
+
+    if (!comment) {
+      console.warn('No comment found for activeCommentId:', this.activeCommentId);
+      return;
+    }
+
+    // Remove all formatting
+    this._removeAllCommentFormatting();
+
+    this._applyCommentFormatting(commentId);
+  }
+
+  _removeAllCommentFormatting() {
+    // Remove formatting from all comments
+    this.comments.forEach((comment, commentId) => {
+      this._removeCommentFormatting(commentId);
+    });
+    this.activeCommentId = null;
+  }
+
+  _removeCommentFormatting(commentId: string) {
+    const comment = this.comments.get(commentId);
+
+    if (!comment) {
+      console.warn('No comment found for activeCommentId:', this.activeCommentId);
+      return;
+    }
+
+    try {
+      const encodedRelativePos = comment.get('relativePos');
+
+      if (!encodedRelativePos) {
+        console.warn('No relativePos found for comment:', this.activeCommentId);
+      }
+
+      // Decode relative position
+      const relativePos = Y.decodeRelativePosition(encodedRelativePos);
+      const absolutePos = Y.createAbsolutePositionFromRelativePosition(relativePos, this.ydoc);
+
+      if (absolutePos && absolutePos.type === this.yText) {
+        const index = absolutePos.index;
+        const length = comment.get('selectionLength');
+        this.quill.formatText(index, length, { background: false }, 'user');
+        this.quill.setSelection(index + length);
+      }
+
+      this.activeCommentId = null;
+    } catch (err) {
+      console.error('Failed to remove comment formatting:', err);
+    }
+  }
+
+  _applyCommentFormatting(commentId: string) {
+    const comment = this.comments.get(commentId);
+
+    if (!comment) {
+      console.warn('No comment found for activeCommentId:', this.activeCommentId);
+      return;
+    }
+
+    try {
+      const encodedRelativePos = comment.get('relativePos');
+
+      if (!encodedRelativePos) {
+        console.warn('No relativePos found for comment:', commentId);
+        throw new Error('No relativePos found');
+      }
+
+      // Decode the relative position
+      const relativePos = Y.decodeRelativePosition(encodedRelativePos);
+      const absolutePos = Y.createAbsolutePositionFromRelativePosition(relativePos, this.ydoc);
+
+      // Check if same instance of Y.Text
+      if (absolutePos && absolutePos.type === this.yText) {
+        const index = absolutePos.index;
+        const length = comment.get('selectionLength');
+
+        // Add background and set selection to END of commented text
+        this.quill.formatText(index, length, { background: '#d9ff0048' }, 'user');
+        this.quill.setSelection(index + length + 1);
+      }
+    } catch (e) {
+      console.error('Failed to apply comment formatting:', e);
+      this.quill.setSelection(0);
+      this.activeCommentId = null;
+      return;
+    }
   }
 }
